ffa2948cbf20ff3e45fb921352012ec7cb575b14
Ilya <ilya.lebedev.2003@mail.ru>
2016-07-11 13:58:42 +0600

    benefit: fix applying patch

--- patch/apply-patch.sh
+++ patch/apply-patch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd ../../../..
+cd ../../..
 cd device/cyanogen/msm8916-common
-git apply -v ../../device/ark/benefit/patch/nodtb.patch
+git apply -v ../../../device/ark/benefit/patch/nodtb.patch
 cd ../../..
 echo Patch Applied Successfully!
