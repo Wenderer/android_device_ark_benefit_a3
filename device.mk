1f7e8436e83d5759cf79ac2be6f14636eb6e045f
Ilya <ilya.lebedev.2003@mail.ru>
2016-07-11 09:44:29 +0600

    benefit: add keylayout for gt9xx

--- device.mk
+++ device.mk
@@ -138,8 +138,7 @@ PRODUCT_COPY_FILES += \
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
-    $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl
+    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/Goodix-CTP.kl
 
 # Lights
 PRODUCT_PACKAGES += \
